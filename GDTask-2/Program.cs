7ddfbf2e1f00727ca52ae6c24d03fc6df94a8e47
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2023-04-18 17:25:02 +0300

    Добавьте файлы проекта.

---
+++ GDTask-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDTask_2
+{
+    internal class Program
+    {
+        static int getTask2(object[] array ) {
+            int answer = 0;
+            
+            foreach(var item in array)
+            {
+                if (item is String) 
+                {
+                    answer--;
+                    continue;
+                }
+                answer += (int)item;
+            }
+            return answer;
+
+        }
+        static void Main(string[] args)
+        {
+            object[] array = { };
+            Console.WriteLine(getTask2(array));
+            Console.ReadLine();
+        }
+    }
+}
